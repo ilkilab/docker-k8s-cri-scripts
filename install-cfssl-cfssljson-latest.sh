88d4ee913a5ff891515e6e4aa75262fa16c3c0ce
pierre <43336050+pierreilki@users.noreply.github.com>
2019-07-10 10:10:17 +0200

    Update install-cfssl-cfssljson-latest.sh

--- install-cfssl-cfssljson-latest.sh
+++ install-cfssl-cfssljson-latest.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 go get -u github.com/cloudflare/cfssl/cmd/cfssl
 go get -u github.com/cloudflare/cfssl/cmd/cfssljson
+echo 'PATH=$PATH/root/go/bin' >> ~/.bash_profile
+source ~/.bash_profile
