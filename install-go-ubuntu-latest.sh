490b126d6318f167a4ca818731e20db2379c2581
pierre <43336050+pierreilki@users.noreply.github.com>
2019-07-10 09:43:20 +0200

    Update install-go-ubuntu-latest.sh

--- install-go-ubuntu-latest.sh
+++ install-go-ubuntu-latest.sh
@@ -1,3 +1,7 @@
 sudo add-apt-repository ppa:longsleep/golang-backports
 sudo apt-get update
 sudo apt-get install golang-go
+
+echo 'export GOPATH=$HOME/go' >> ~/.bash_profile
+source ~/.bash_profile
+
